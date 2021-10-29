d51481581537f54a8811fd12c45ac631427405a8
ArindamBiswas-dev <arindambiswas616@gmail.com>
2021-10-29 09:20:25 +0530

    feat: add allSnippetSlice and singleSnippetSlice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import addSnippetSlice from '../features/addSnippetSlice'
+import allSnippetSlice from '../features/allSnippetSlice'
 import pinnedReducer from '../features/pinnedSnippetSlice'
 import searchReducer from '../features/searchSlice'
+import singleSnippetSlice from '../features/singleSnippetSlice'
 
 export const store = configureStore({
   reducer: {
     pinnedSnippet: pinnedReducer,
     searchSnippet: searchReducer,
     addSnippet: addSnippetSlice,
+    allSnippet: allSnippetSlice,
+    singleSnippet: singleSnippetSlice,
   },
 })
 
